bc5341b7ef86316b8a2d95864e7baa8b583e96be
NavodContrath <navod.contrath@gmail.com>
2025-02-17 16:40:26 +0100

    add 4th task

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+let wordsVowels = 0
 
 // Dichiara la funzione qui.
-
+function countVowels() {
+    for (let i = 0; i < word.length; i++) {
+        const thisLetter = word[i]
+        if ((thisLetter == "a") || (thisLetter == "i") || (thisLetter == "e") || (thisLetter == "o") || (thisLetter == "u")) {
+            wordsVowels += 1
+        }
+    }
+    return wordsVowels
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+countVowels()
+console.log(wordsVowels);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
