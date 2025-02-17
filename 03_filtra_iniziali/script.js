da3b35cfaa8869f181feb106ac9a849db985e2af
NavodContrath <navod.contrath@gmail.com>
2025-02-17 16:34:29 +0100

    add 3rd task

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const emptyNames = []
 
 // Dichiara la funzione qui.
-
-
+function filterByFirstletter() {
+    for (let i = 0; i < names.length; i++) {
+        thisName = names[i]
+        if (thisName.charAt(0) == "A") {
+            emptyNames.push(thisName)
+        }
+    }
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+filterByFirstletter()
+console.log(emptyNames);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
