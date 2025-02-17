339e80cee9761af6452e0c120f15bd926e2b6f61
NavodContrath <navod.contrath@gmail.com>
2025-02-17 17:09:25 +0100

    add bonus task

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,31 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+const date = new Date()
 
 // Dichiara la funzione qui.
-
-
+function greetings() {
+    if (date <= date.getHours(13)) {
+        console.log(`Buongiorno ${name}`);
+    } else if (date > date.getHours(13) && date < date.getHours(17)) {
+        console.log(`Buon pomeriggio ${name}`);
+    } else {
+        console.log(`Buonasera ${name}`);
+    }
+}
+/* 
+const greetings =()=>{
+     if (date <= date.getHours(13)) {
+        console.log(`Buongiorno ${name}`);
+    } else if (date > date.getHours(13) && date < date.getHours(17)) {
+        console.log(`Buon pomeriggio ${name}`);
+    } else {
+        console.log(`Buonasera ${name}`);
+    }
+}
+*/
 // Invoca la funzione qui e stampa il risultato in console
-
+greetings()
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
