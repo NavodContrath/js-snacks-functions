907e626ae5a05d1d332192f78dbeb1e1070bdeaf
NavodContrath <navod.contrath@gmail.com>
2025-02-17 16:56:34 +0100

    add arrow function 3rd task

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,7 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 const emptyNames = []
 
 // Dichiara la funzione qui.
-function filterByFirstletter() {
+/* function filterByFirstletter() {
+    for (let i = 0; i < names.length; i++) {
+        thisName = names[i]
+        if (thisName.charAt(0) == "A") {
+             emptyNames.push(thisName)
+       }
+    }
+}
+*/
+const filterByFirstletter = () => {
     for (let i = 0; i < names.length; i++) {
         thisName = names[i]
         if (thisName.charAt(0) == "A") {
