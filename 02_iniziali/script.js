ba17366234072d0b21421721d075bdb8c359af64
NavodContrath <navod.contrath@gmail.com>
2025-02-17 16:25:11 +0100

    add 2nd task

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,12 +1,19 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const empty_name = []
 
 // Dichiara la funzione qui.
-
+function firstLetter() {
+    for (let i = 0; i < names.length; i++) {
+        const thisName = names[i]
+        empty_name.push(thisName.charAt(0))
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+firstLetter()
+console.log(empty_name);
 
 
 
