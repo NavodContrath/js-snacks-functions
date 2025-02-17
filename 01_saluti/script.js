2c6d075a92e3033ccc892ae58b8a8c2ea6505e4e
NavodContrath <navod.contrath@gmail.com>
2025-02-17 15:34:20 +0100

    add task 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greetings(name) {
+    console.log(`Greetings ${name}`);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+greetings(userName)
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
